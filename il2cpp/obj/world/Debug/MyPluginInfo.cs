d1bcb1d003f5e650d914ff1fb2b1275c5662148b
ohmymods <ohmymods@local>
2026-08-13 01:37:50 +0800

    chore: gitignore dotnet 构建产物

--- il2cpp/obj/world/Debug/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace KingdomEnhancedMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "KingdomEnhancedMod";
-        public const string PLUGIN_NAME = "KingdomEnhancedMod";
-        public const string PLUGIN_VERSION = "2.4.0";
-    }
-}
